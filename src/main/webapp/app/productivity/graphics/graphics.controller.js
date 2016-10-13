46309d2a5b50cc22b6550972946dc22c385ffa87
olegpoly <olegpoly@gmail.com>
2016-10-13 14:35:30 +0000

    add bar chart support (partly)

--- src/main/webapp/app/productivity/graphics/graphics.controller.js
+++ src/main/webapp/app/productivity/graphics/graphics.controller.js
@@ -15,9 +15,13 @@
             ActivityService.getActivitiesByDateRange(startDate, endDate).then(function (result) {
                 $scope.labels = [];
                 $scope.data = [];
+                $scope.barChartLabels = [];
+                $scope.barChartSeries = [];
+                $scope.barChartData = [];
 
                 var activitiesExecutions = result.data;
                 var arr = {};
+                var dates = {};
 
                 for (var i = 0; i < activitiesExecutions.length; i++) {
                     var activityName = activitiesExecutions[i].activity.name;
@@ -25,16 +29,41 @@
                         var ac = arr[activityName];
                         ac.count++;
                     } else {
-                        arr[activityName] = { count: 1 };
+                        arr[activityName] = {count: 1};
                     }
+
+                    var activityDate = activitiesExecutions[i].executionDate;
+                    dates[activityDate] = {};
+                    dates[activityDate][activityName] = 1;
                 }
 
                 for (var prop in arr) {
                     if (arr.hasOwnProperty(prop)) {
                         $scope.labels.push(prop + "(times)");
                         $scope.data.push(arr[prop].count);
+
+                        $scope.barChartSeries.push(prop);
+                        var a = [];
+
+                        for (var date = startDate; date < endDate; date.setDate(date.getDate() + 1)) {
+                            $scope.barChartLabels.push(date);
+                            if (dates[date]) {
+                                if (dates[date][prop]) {
+                                    a.push(100);
+                                } else {
+                                    a.push(0);
+                                }
+                            } else {
+                                a.push(0);
+                            }
+                        }
+
+                        $scope.barChartData.push(a);
                     }
                 }
+
+                //
+
             });
         });
 
